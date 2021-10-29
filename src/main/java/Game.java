faad4ba5b7b4ecac4679959e0e5d6bad730991b7
ybw903 <conner903@naver.com>
2021-10-29 22:41:04 +0900

    feat: game play

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,3 +1,9 @@
 public class Game {
-
+    public void play() {
+        System.out.println("======== Blackjack Game========");
+        Dealer dealer = new Dealer();
+        Gamer gamer = new Gamer();
+        Rule rule = new Rule();
+        CardDeck cardDeck = new CardDeck();
+    }
 }
