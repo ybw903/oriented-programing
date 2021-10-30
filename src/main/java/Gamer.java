182cb7da6075cf748a010cd97393fcfc577c93d7
ybw903 <conner903@naver.com>
2021-10-30 17:48:31 +0900

    feat: gamer

--- src/main/java/Gamer.java
+++ src/main/java/Gamer.java
@@ -1,2 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Gamer {
+    private List<Card> cards;
+
+    public Gamer() {
+        cards = new ArrayList<>();
+    }
+
+    public void receiveCard(Card card) {
+        this.cards.add(card);
+    }
+
+    public List<Card> openCards() {
+        return this.cards;
+    }
+
+    public void showCards() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("헌재 보유 카드 목록\n");
+
+        for (Card card : cards) {
+            sb.append(card.toString());
+            sb.append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
 }
