a6beab5ff78e8aafb021b54f5dc150e903718881
ybw903 <conner903@naver.com>
2021-10-30 13:57:08 +0900

    feat: generate cards

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,2 +1,33 @@
 public class Card {
+    private String denomination;
+    private String pattern;
+
+    public String getDenomination() {
+        return denomination;
+    }
+
+    public Card(String denomination, String pattern) {
+        this.denomination = denomination;
+        this.pattern = pattern;
+    }
+
+    public void setDenomination(String denomination) {
+        this.denomination = denomination;
+    }
+
+    public String getPattern() {
+        return pattern;
+    }
+
+    public void setPattern(String pattern) {
+        this.pattern = pattern;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "denomination='" + denomination + '\'' +
+                ", pattern='" + pattern + '\'' +
+                '}';
+    }
 }
--- src/main/java/CardDeck.java
+++ src/main/java/CardDeck.java
@@ -2,5 +2,48 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CardDeck {
+    private static final String[] PATTERNS = {"spade", "heart", "diamond", "club"};
+    private static final int CARD_COUNT = 13;
+    private List<Card> cards;
 
+    public CardDeck() {
+        this.cards = generateCards();
+    }
+
+    public List<Card> generateCards() {
+        List<Card> cards = new ArrayList<>();
+        for(String pattern : PATTERNS) {
+            for (int i = 1; i <= CARD_COUNT; i++) {
+                String denomination = numberToDenomination(i);
+                Card card = new Card(pattern, denomination);
+
+                cards.add(card);
+            }
+        }
+        return cards;
+    }
+
+    private String numberToDenomination(int number) {
+        if (number == 1) {
+            return "A";
+        } else if (number == 11) {
+            return "J";
+        } else if (number == 12) {
+            return "Q";
+        } else if (number == 13) {
+            return "K";
+        }
+        return String.valueOf(number);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        for(Card card : cards) {
+            sb.append(card.toString());
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
 }
